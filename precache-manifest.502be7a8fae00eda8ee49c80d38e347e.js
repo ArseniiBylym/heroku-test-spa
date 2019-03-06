d320344c3b88dedc8905ee2953389182ab9bc1b7
Arsenii Bylym <arseniibylym@gmail.com>
2019-03-06 20:56:52 +0200

    Updates

---
+++ precache-manifest.502be7a8fae00eda8ee49c80d38e347e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5051134e5d84ec7828c7",
+    "url": "/heroku-spa-test/static/js/runtime~main.5051134e.js"
+  },
+  {
+    "revision": "7b4698f7678a24f337bd",
+    "url": "/heroku-spa-test/static/js/main.7b4698f7.chunk.js"
+  },
+  {
+    "revision": "2888d8c73c46992dee39",
+    "url": "/heroku-spa-test/static/js/2.2888d8c7.chunk.js"
+  },
+  {
+    "revision": "7b4698f7678a24f337bd",
+    "url": "/heroku-spa-test/static/css/main.018fdcde.chunk.css"
+  },
+  {
+    "revision": "12bd6f0c65b07b5bccf6830a2c2579b9",
+    "url": "/heroku-spa-test/index.html"
+  }
+];
