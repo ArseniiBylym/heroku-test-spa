7fa3b5ac571c1b9344cfbb928a0bd316af2038dd
Arsenii Bylym <arseniibylym@gmail.com>
2019-03-06 21:21:44 +0200

    setup for heroku

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         Main app component
-        <Router basename="/heroku-test-spa">
+        <Router >
           <div>
 
           <div><Link to='/home' >home</Link></div>
